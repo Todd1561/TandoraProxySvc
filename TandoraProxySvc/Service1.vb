e5aa6da1307ad5caac2f717459abc8dcdabfefaf
Todd Nelson <todd@toddnelson.net>
2021-02-14 23:51:47 -0500

    initial commit

---
+++ TandoraProxySvc/Service1.vb
@@ -0,0 +1,255 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports System.Threading
+Public Class Service1
+
+    Dim pianobarStations As New ArrayList, pSong As String = "", pAlbum As String = "", pArtist As String = "", pStation As String = ""
+    Dim cmd As String = "", pCurTime As String = "/", pIsPlaying As Boolean = False, pianobarLast As String = ""
+    Dim pianoPath As String = AppDomain.CurrentDomain.BaseDirectory & "Pianobar.exe", pianoServ As String = "localhost", pianoPort As String = "23"
+    Dim username As String = "", password As String = "", tandoraPort As String = "1561"
+    Dim tcpListener
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        Dim help As String = "TandoraProxy, Ver. 2.5 (2/14/2021), toddnelson.net.  https://toddnelson.net" & vbCrLf & vbCrLf &
+            "Place a tandoraproxy.cfg file in the same directory as TandoraProxySvc.exe with the following settings: " & vbCrLf & vbCrLf &
+            "pianopath=<absolute path to Pianobar exe> (default: current directory)" & vbCrLf &
+            "pianoserv=<address of Pianobar Telnet server> (default: localhost)" & vbCrLf &
+            "pianoport=<TCP port of Pianobar Telnet server> (default: 23)" & vbCrLf &
+            "tandoraport=<TCP port for TandoraProxy> (default: 1561)" & vbCrLf &
+            "username=<Pianobar Telnet server username> (Required)" & vbCrLf &
+            "password=<Pianobar Telnet server password> (Required)"
+
+        If Not EventLog.SourceExists("TandoraProxy") Then EventLog.CreateEventSource("TandoraProxy", "Application")
+
+        Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory)
+
+        If Not File.Exists("tandoraproxy.cfg") Then
+            EventLog.WriteEntry("TandoraProxy", help, EventLogEntryType.Error, 1, 1)
+            [Stop]()
+            Exit Sub
+        End If
+
+        For Each line As String In File.ReadLines("tandoraproxy.cfg")
+            If line.Substring(0, line.IndexOf("=") + 1) = "pianopath=" And line.Length > 10 Then pianoPath = line.Substring(line.IndexOf("=") + 1)
+            If line.Substring(0, line.IndexOf("=") + 1) = "pianoserv=" And line.Length > 10 Then pianoServ = line.Substring(line.IndexOf("=") + 1)
+            If line.Substring(0, line.IndexOf("=") + 1) = "pianoport=" And line.Length > 10 Then pianoPort = line.Substring(line.IndexOf("=") + 1)
+            If line.Substring(0, line.IndexOf("=") + 1) = "tandoraport=" And line.Length > 12 Then tandoraPort = line.Substring(line.IndexOf("=") + 1)
+            If line.Substring(0, line.IndexOf("=") + 1) = "username=" And line.Length > 9 Then username = line.Substring(line.IndexOf("=") + 1)
+            If line.Substring(0, line.IndexOf("=") + 1) = "password=" And line.Length > 9 Then password = line.Substring(line.IndexOf("=") + 1)
+        Next line
+
+        If username = "" Or password = "" Then
+            EventLog.WriteEntry("TandoraProxy", "Username and password required." & vbCrLf & vbCrLf & help, EventLogEntryType.Error, 1, 1)
+            [Stop]()
+            Exit Sub
+        End If
+
+        If Not Regex.IsMatch(pianoPath.ToLower, "[a-z]:.*") Then
+            EventLog.WriteEntry("TandoraProxy", "Path to Pianobar.exe needs to be a local drive letter.", EventLogEntryType.Error, 1, 1)
+            [Stop]()
+            Exit Sub
+        End If
+
+        'Create new thread to listen for TandoraProxy clients 
+        'run in the background so they auto-terminate when the service stops
+        Dim thdControl As Thread = New Thread(AddressOf ListenForClients) With {.IsBackground = True}
+        thdControl.Start()
+
+        'create new thread to send commands to pianobar and monitor responses
+        Dim thdMonitorPianobar As Thread = New Thread(AddressOf MonitorPianobar) With {.IsBackground = True}
+        thdMonitorPianobar.Start()
+
+    End Sub
+
+    Sub MonitorPianobar()
+        Dim tc As MinimalisticTelnet.TelnetConnection = New MinimalisticTelnet.TelnetConnection(pianoServ, pianoPort)
+        Dim s As String = tc.Login(username, password, 600)
+        Console.Write(s)
+        Dim prompt As String = s.TrimEnd()
+        prompt = s.Substring(prompt.Length - 1, 1)
+        If prompt <> "$" AndAlso prompt <> ">" Then Throw New Exception("Connection failed")
+        prompt = ""
+
+        If tc.IsConnected Then
+            tc.WriteLine(pianoPath.Substring(0, 2))
+            tc.WriteLine("""" & pianoPath & """")
+            Threading.Thread.Sleep(3000)
+
+            'set pianobar to "high" priroty to avoid stuttering audio
+            Process.Start("wmic", "process where name=""pianobar.exe"" CALL setpriority 128")
+
+            Dim resp As String = ""
+
+            'keep looping, taking response from pandora and sending commands as I get them from the API
+            'commands can be broken up with pipes and comas injected to create delays. 
+            '(useful for changing stations And needing to wait a split second for list to build in pianobar UI)
+            While True
+
+                If cmd <> "" Then
+                    For Each c As String In cmd.Split("|")
+                        If c = "," Then Thread.Sleep(50) Else tc.Write(c)
+                    Next
+
+                    cmd = ""
+                End If
+
+                resp = tc.Read
+
+                If resp <> "" Then
+
+                    'parse out station list
+                    If Regex.IsMatch(resp, "\s(\d+)\).{5}(.*?)\n") Then
+                        For Each station As Match In Regex.Matches(resp, "\s(\d+)\).{5}(.*?)\n")
+
+                            'Detected the first station, clear the array
+                            If station.Groups(1).Value.Trim = "0" Then pianobarStations.Clear()
+                            pianobarStations.Add(station.Groups(2).Value.Trim)
+
+                        Next
+                    End If
+
+                    'parse out current song info
+                    If Regex.IsMatch(resp, "\|\>  ""(.*?)"" by ""(.*?)"" on ""(.*?)""") Then
+                        Dim m As Match = Regex.Match(resp, "\|\>  ""(.*?)"" by ""(.*?)"" on ""(.*?)""")
+                        pSong = m.Groups(1).Value
+                        pArtist = m.Groups(2).Value
+                        pAlbum = m.Groups(3).Value
+                    End If
+
+                    'parse out current station
+                    If Regex.IsMatch(resp, "\|\>  Station ""(.*?)""") Then
+                        Dim m As Match = Regex.Match(resp, "\|\>  Station ""(.*?)""")
+                        pStation = m.Groups(1).Value
+                    End If
+
+                    'parse out current play time
+                    If Regex.IsMatch(resp, "#\s+([-\d:]+)/([-\d:]+)") Then
+                        Dim lastPCurTime As String = pCurTime
+                        Dim m As Match = Regex.Match(resp, "#\s+([-\d:]+)/([-\d:]+)")
+                        pCurTime = m.Groups(1).Value & "/" & m.Groups(2).Value
+                        If lastPCurTime <> pCurTime Then pIsPlaying = True Else pIsPlaying = False
+                    End If
+
+                    Console.Write(resp)
+                    pianobarLast = resp
+                End If
+
+            End While
+
+        End If
+
+        While tc.IsConnected AndAlso prompt.Trim() <> "exit"
+            Console.Write(tc.Read())
+            prompt = Console.ReadLine()
+            tc.WriteLine(prompt)
+            Console.Write(tc.Read())
+        End While
+
+        Console.WriteLine("***DISCONNECTED")
+        Console.ReadLine()
+    End Sub
+
+    Sub ListenForClients()
+        tcpListener = New TcpListener(IPAddress.Any, tandoraPort)
+
+        tcpListener.Start()
+
+        While True
+            'blocks until a client has connected to the server
+            Dim client As TcpClient = tcpListener.AcceptTcpClient
+            'create a thread to handle communication with connected client
+            Dim clientThread As Thread = New Thread(New ParameterizedThreadStart(AddressOf HandleClientComm))
+            clientThread.Start(client)
+        End While
+
+    End Sub
+
+    Private Sub HandleClientComm(ByVal client As Object)
+        Try
+            Dim tcpClient As TcpClient = CType(client, TcpClient)
+            Dim clientStream As NetworkStream = tcpClient.GetStream
+            Dim message() As Byte = New Byte((4096) - 1) {}
+            Dim encoder As ASCIIEncoding = New ASCIIEncoding
+            Dim bytesRead As Integer
+            Dim fullMsg As String = ""
+
+            While True
+                bytesRead = 0
+                Try
+                    'blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096)
+                Catch ex As System.Exception
+                    'a socket error has occured
+                    Exit While
+                End Try
+
+                fullMsg += encoder.GetString(message, 0, bytesRead)
+
+                If (bytesRead = 0) Or fullMsg.Contains(vbCrLf) Then
+                    'the client has disconnected from the server
+                    fullMsg = fullMsg.Replace(vbCrLf, "")
+                    Exit While
+                End If
+
+            End While
+
+            Console.WriteLine("Command Received: """ & fullMsg & """ (" & Now & ")")
+
+            If fullMsg.Contains("change station") Then
+
+                'Capture current song info so I know when pianobar has finished selecting new song
+                Dim curPianobarDur As String = pCurTime.Substring(pCurTime.IndexOf("/"))
+
+                'See if song selected, if so need to press 's' first to change station
+                If pianobarLast.Substring(0, 3) = "#  " Then cmd = "s|,|"
+                cmd += pianobarStations.IndexOf(fullMsg.Substring(15)) & vbCrLf
+
+                'Wait for pianobar to select song and start playing
+                Do Until curPianobarDur <> pCurTime.Substring(pCurTime.IndexOf("/"))
+                    Threading.Thread.Sleep(10)
+                Loop
+            ElseIf fullMsg.Contains("playpause") Then
+                cmd = "p"
+            ElseIf fullMsg.Contains("next") Then
+                Dim curPianobarDur As String = pCurTime.Substring(pCurTime.IndexOf("/"))
+                cmd = "n"
+                Do Until curPianobarDur <> pCurTime.Substring(pCurTime.IndexOf("/"))
+                    Threading.Thread.Sleep(10)
+                Loop
+            ElseIf fullMsg.Contains("thumbsdown") Then
+                Dim curPianobarDur As String = pCurTime.Substring(pCurTime.IndexOf("/"))
+                cmd = "-"
+                Do Until curPianobarDur <> pCurTime.Substring(pCurTime.IndexOf("/"))
+                    Threading.Thread.Sleep(100)
+                Loop
+            ElseIf fullMsg.Contains("thumpsup") Then
+                cmd = "+"
+            End If
+
+            If Trim(pStation) = "" Then pStation = "** No Station Selected **"
+            If Trim(pSong) = "" Then pSong = "** No Song Selected **"
+
+            Dim sendStr As String = "IS PLAYING: " & pIsPlaying & vbCrLf &
+                                    "STATION LIST: " & String.Join("|", pianobarStations.ToArray) & vbCrLf &
+                                    "CURRENT STATION: " & pStation & vbCrLf &
+                                    "CURRENT SONG: """ & pSong.Replace(ChrW(233), "e") & """ by """ & pArtist & """ on """ & pAlbum & """" & vbCrLf &
+                                    "CURRENT TIME: " & pCurTime & vbCrLf
+
+            Dim sendBytes As Byte() = Encoding.ASCII.GetBytes(sendStr)
+            clientStream.Write(sendBytes, 0, sendBytes.Length)
+
+            tcpClient.Close()
+        Catch e As Exception
+            'Console.WriteLine(e.StackTrace)
+            System.Diagnostics.EventLog.WriteEntry("TandoraProxy", e.Message & vbCrLf & vbCrLf & e.StackTrace, EventLogEntryType.Error)
+        End Try
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        'EventLog.WriteEntry("TandoraProxy", "TandoraProxy stopped", EventLogEntryType.Information, 1, 1)
+    End Sub
+
+End Class
---
+++ TandoraProxySvc/Telnet.vb
@@ -0,0 +1,101 @@
+﻿Imports System.Text
+Imports System.Net.Sockets
+
+Namespace MinimalisticTelnet
+    Enum Verbs
+        WILL = 251
+        WONT = 252
+        [DO] = 253
+        DONT = 254
+        IAC = 255
+    End Enum
+
+    Enum Options
+        SGA = 3
+    End Enum
+
+    Class TelnetConnection
+        Private tcpSocket As TcpClient
+        Private TimeOutMs As Integer = 100
+
+        Public Sub New(ByVal Hostname As String, ByVal Port As Integer)
+            tcpSocket = New TcpClient(Hostname, Port)
+        End Sub
+
+        Public Function Login(ByVal Username As String, ByVal Password As String, ByVal LoginTimeOutMs As Integer) As String
+            Dim oldTimeOutMs As Integer = TimeOutMs
+            TimeOutMs = LoginTimeOutMs
+            Dim s As String = Read()
+            If Not s.TrimEnd().EndsWith(":") Then Throw New Exception("Failed to connect : no login prompt")
+            WriteLine(Username)
+            s += Read()
+            If Not s.TrimEnd().EndsWith(":") Then Throw New Exception("Failed to connect : no password prompt")
+            WriteLine(Password)
+            s += Read()
+            TimeOutMs = oldTimeOutMs
+            Return s
+        End Function
+
+        Public Sub WriteLine(ByVal cmd As String)
+            Write(cmd & vbCrLf)
+        End Sub
+
+        Public Sub Write(ByVal cmd As String)
+            If Not tcpSocket.Connected Then Return
+            Dim buf As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(cmd.Replace(vbNullChar & "xFF", vbNullChar & "xFF" & vbNullChar & "xFF"))
+            tcpSocket.GetStream().Write(buf, 0, buf.Length)
+        End Sub
+
+        Public Function Read() As String
+            If Not tcpSocket.Connected Then Return Nothing
+            Dim sb As StringBuilder = New StringBuilder()
+
+            Do
+                ParseTelnet(sb)
+                System.Threading.Thread.Sleep(TimeOutMs)
+            Loop While tcpSocket.Available > 0
+
+            Return sb.ToString()
+        End Function
+
+        Public ReadOnly Property IsConnected As Boolean
+            Get
+                Return tcpSocket.Connected
+            End Get
+        End Property
+
+        Private Sub ParseTelnet(ByVal sb As StringBuilder)
+            While tcpSocket.Available > 0
+                Dim input As Integer = tcpSocket.GetStream().ReadByte()
+
+                Select Case input
+                    Case -1
+                    Case CInt(Verbs.IAC)
+                        Dim inputverb As Integer = tcpSocket.GetStream().ReadByte()
+                        If inputverb = -1 Then Exit Select
+
+                        Select Case inputverb
+                            Case CInt(Verbs.IAC)
+                                sb.Append(inputverb)
+                            Case CInt(Verbs.[DO]), CInt(Verbs.DONT), CInt(Verbs.WILL), CInt(Verbs.WONT)
+                                Dim inputoption As Integer = tcpSocket.GetStream().ReadByte()
+                                If inputoption = -1 Then Exit Select
+                                tcpSocket.GetStream().WriteByte(CByte(Verbs.IAC))
+
+                                If inputoption = CInt(Options.SGA) Then
+                                    tcpSocket.GetStream().WriteByte(If(inputverb = CInt(Verbs.[DO]), CByte(Verbs.WILL), CByte(Verbs.[DO])))
+                                Else
+                                    tcpSocket.GetStream().WriteByte(If(inputverb = CInt(Verbs.[DO]), CByte(Verbs.WONT), CByte(Verbs.DONT)))
+                                End If
+
+                                tcpSocket.GetStream().WriteByte(CByte(inputoption))
+                            Case Else
+                        End Select
+
+                    Case Else
+                        sb.Append(ChrW(input))
+                End Select
+            End While
+        End Sub
+    End Class
+End Namespace
